ce13215539240d03f907755858ff5015cae00d72
jessHwood <jhorntvedt@gmail.com>
2017-04-28 15:43:20 -0600

    index and show routes back end

--- starter-code/scripts/controllers/cardsController.js
+++ starter-code/scripts/controllers/cardsController.js
@@ -1,14 +1,26 @@
+console.log('is this working?');
+
 angular.module('CardsAgainstAssembly')
   .controller('CardsController', CardsController);
 
-function CardsController(){
-  var vm = this;
-  vm.questionsList = [
-    {question: "What is Batman's guilty pleasure?"},
-    {question: "I'm sorry professor, I couldn't complete my homework because _________."},
-    {question: "I get by with a little help from _________."},
-    {question: "_________. It's a trap!"},
-    {question: "The class field trip was completely ruined by _________."},
-    {question: "What's my secret power?"}
-  ]
+  //CONTROLLERS
+CardsController.$inject = ['$http'];
+
+function CardsController($http){
+  var self = this;
+  self.all = [];
+  self.getCards = getCards;
+
+  function getCards(){
+  $http 
+    .get('https://shielded-forest-41789.herokuapp.com/api/flashcards')
+    .then(function(response){
+      console.log(response.data);
+      console.log("yo");
+      self.all = (response.data);
+    });   
+}
+  getCards();
+
+
 }
